307d3f50fa15d18e04176b3389ac6aabf338fecc
Admin <Admin@TEB12703>
2023-01-13 11:08:07 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgiftnångot
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriva in 3 städer");
+            string[,] städer = new string[3,3];
+            int u = 1;
+            for(int i=0; i < städer.Length/3; i++)
+            {
+                for (int j = 0; j < städer.Length/3; j++)
+                {
+                    Console.WriteLine($"skriva in städerna " +
+                        $"{u}: "); städer[i, j] = Console.ReadLine();
+                    u++;
+                }
+            }
+            Console.WriteLine("de städer du har skrivit in är");
+
+            foreach (string städ in städer)
+            {
+                Console.WriteLine(städ);
+            }
+
+        }
+    }
+}
